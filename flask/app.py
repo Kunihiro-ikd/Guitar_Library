9812fff19b393797ce3b444376b0fda9d9e220ef
Kunihiro-ikd <kunikuni6129.sddj@gmail.com>
2022-05-03 17:49:28 -0700

    app.pyを元に戻す

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
-import logging
+# import logging
 
 app = Flask(__name__, static_folder='../vue_template/dist/static', template_folder='../vue_template/dist')
 # https://qiita.com/KWS_0901/items/7163e52b4041b909f5bc
 
-app.logger.setLevel(logging.DEBUG)
-log_handler = logging.FileHandler(LOGFILE_NAME)
-log_handler.setLevel(logging.DEBUG)
-app.logger.addHandler(log_handler)
-app.logger.info(11111111)
 
 @app.route('/',defaults={'path':''})
 @app.route('/<path:path>')
